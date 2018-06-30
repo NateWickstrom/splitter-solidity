c13ae0aa7e311d4eb7978a8e74ee187adb6097ee
Nate Wickstrom <nwickstrom@pandora.com>
2018-06-30 15:42:50 -0700

    removing some extra functionality for now

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
 var Ownable = artifacts.require("./Ownable.sol");
-var Pausable = artifacts.require("./Pausable.sol");
-var OwnedPausable = artifacts.require("./OwnedPausable.sol");
+//var Pausable = artifacts.require("./Pausable.sol");
+//var OwnedPausable = artifacts.require("./OwnedPausable.sol");
 var Splitter = artifacts.require("./Splitter.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Ownable);
-  deployer.deploy(Pausable);
-  deployer.deploy(OwnedPausable);
+  //deployer.deploy(Pausable);
+  //deployer.deploy(OwnedPausable);
   deployer.deploy(Splitter);
 };
